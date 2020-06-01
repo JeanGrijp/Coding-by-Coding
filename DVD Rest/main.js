44e7653700f59a4b8335af4e034e3f9a3519bf04
Jean Grijp <grijpjean@gmail.com>
2020-06-01 20:08:06 -0300

    update

--- DVD Rest/main.js
+++ DVD Rest/main.js
@@ -1,19 +1,19 @@
+const {floor, random} = Math
+const valorRandom = (from, to) => {
+  return floor(random() * (to - from + 1) + 1)
+}
 const gel = (element) => document.querySelector(element);
-
 const ball = gel('.ball');
 const container = gel('.container');
-
 const convert = (text) => +text.split('px').join('');
-
 const screenWidth = document.body.offsetWidth;
 const screenHeight = convert(getComputedStyle(container).height);
 const ballWidth = convert(getComputedStyle(ball).width);
 
 let positionX = 0;
-let speedX = 10;
-
+let speedX = 1;
 let positionY = 0;
-let speedY = 8;
+let speedY = 1;
 
 setInterval(() => {
   positionX += speedX;
@@ -24,6 +24,7 @@ setInterval(() => {
     || positionX <= 0
   ) {
     speedX *= -1;
+    ball.style.backgroundColor = 'rgb('+`${valorRandom(0, 255)}`+","+ `${valorRandom(0, 255)}`+","+ `${valorRandom(0, 255)})`
   }
   
   if (
@@ -31,6 +32,7 @@ setInterval(() => {
     || positionY <= 0
   ) {
     speedY *= -1;
+    ball.style.backgroundColor = 'rgb('+`${valorRandom(0, 255)}`+","+ `${valorRandom(0, 255)}`+","+ `${valorRandom(0, 255)})`
   }
 
   ball.style.left = `${positionX}px`;
